b3205d2d7036fc64beb3a96936237903462bbce4
simone-poli <polismn97@gmail.com>
2025-06-04 14:33:59 +0200

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function vowels(names) {
+    const firstLetter = [];
 
+    for (let i = 0; i < names.length; i++) {
+        firstLetter.push(names[i].charAt(0));
+    }
+
+    return firstLetter;
+
+}
+const result = vowels(names);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(result);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
